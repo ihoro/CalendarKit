f553f56369344d902bef42723e87cb993e44d5df
Igor Ostapenko <ihoro@users.noreply.github.com>
2017-12-01 15:26:38 +0200

    Fix DayView moving
    For example, upon initial date change if current one is not an option.

--- Source/Header/DayHeaderView.swift
+++ Source/Header/DayHeaderView.swift
@@ -99,23 +99,32 @@ extension DayHeaderView: DaySelectorDelegate {
 extension DayHeaderView: DayViewStateUpdating {
   public func move(from oldDate: Date, to newDate: Date) {
     let newDate = newDate.dateOnly()
-    let centerDaySelector = pagingScrollView.reusableViews[1]
-    let startDate = centerDaySelector.startDate.dateOnly()
+    let centerView = pagingScrollView.reusableViews[1]
+    let startDate = centerView.startDate.dateOnly()
 
     let daysFrom = newDate.days(from: startDate, calendar: calendar)
     let newStartDate = beginningOfWeek(newDate)
 
+    let leftView = pagingScrollView.reusableViews[0]
+    let rightView = pagingScrollView.reusableViews[2]
     if daysFrom < 0 {
-      pagingScrollView.reusableViews[0].startDate = newStartDate
       currentWeekdayIndex = abs(daysInWeek + daysFrom % daysInWeek) % daysInWeek
-      pagingScrollView.scrollBackward()
+      centerView.startDate = newStartDate
+      centerView.selectedIndex = currentWeekdayIndex
+      leftView.startDate = centerView.startDate.add(TimeChunk.dateComponents(weeks: -1))
+      rightView.startDate = centerView.startDate.add(TimeChunk.dateComponents(weeks: 1))
+      //pagingScrollView.scrollBackward()
     } else if daysFrom > daysInWeek - 1 {
-      pagingScrollView.reusableViews[2].startDate = newStartDate
       currentWeekdayIndex = daysFrom % daysInWeek
-      pagingScrollView.scrollForward()
+      centerView.startDate = newStartDate
+      centerView.selectedIndex = currentWeekdayIndex
+      leftView.startDate = centerView.startDate.add(TimeChunk.dateComponents(weeks: -1))
+      rightView.startDate = centerView.startDate.add(TimeChunk.dateComponents(weeks: 1))
+      //pagingScrollView.scrollForward()
     } else {
-      centerDaySelector.selectedDate = newDate
       currentWeekdayIndex = daysFrom
+      centerView.selectedDate = newDate
+      centerView.selectedIndex = currentWeekdayIndex
     }
   }
 }
